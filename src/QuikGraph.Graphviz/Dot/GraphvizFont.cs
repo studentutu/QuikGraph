3ddf44f933ee330386f91e12704d0c1d66faac5f
Alexandre Rabérin <kernelith@live.fr>
2020-11-02 21:04:42 +0100

    [ci skip] Add deserialization callback for serializable structures.

--- src/QuikGraph.Graphviz/Dot/GraphvizFont.cs
+++ src/QuikGraph.Graphviz/Dot/GraphvizFont.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+#if SUPPORTS_SERIALIZATION
+using System.Runtime.Serialization;
+#endif
 using JetBrains.Annotations;
 
 namespace QuikGraph.Graphviz.Dot
@@ -10,11 +13,15 @@ namespace QuikGraph.Graphviz.Dot
     [Serializable]
 #endif
     public sealed class GraphvizFont
+#if SUPPORTS_SERIALIZATION
+        : IDeserializationCallback
+#endif
     {
         /// <summary>
         /// Font name.
         /// <see href="https://www.graphviz.org/doc/info/attrs.html#d:fontname">See more</see>
         /// </summary>
+        [NotNull]
         public string Name { get; }
 
         /// <summary>
@@ -30,13 +37,31 @@ namespace QuikGraph.Graphviz.Dot
         /// <param name="sizeInPoints">Font size.</param>
         public GraphvizFont([NotNull] string name, float sizeInPoints)
         {
-            if (string.IsNullOrEmpty(name))
-                throw new ArgumentException("Font name cannot be null or empty.", nameof(name));
-            if (sizeInPoints <= 0)
-                throw new ArgumentOutOfRangeException(nameof(sizeInPoints), "Size must be positive.");
+            ValidateContract(name, sizeInPoints);
 
             Name = name;
             SizeInPoints = sizeInPoints;
         }
+
+        [AssertionMethod]
+        private static void ValidateContract([NotNull] string name, float sizeInPoints)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("Font name cannot be null or empty.", nameof(name));
+            if (sizeInPoints <= 0)
+                throw new ArgumentOutOfRangeException(nameof(sizeInPoints), "Size must be positive.");
+        }
+
+#if SUPPORTS_SERIALIZATION
+        #region IDeserializationCallback
+
+        /// <inheritdoc />
+        void IDeserializationCallback.OnDeserialization(object sender)
+        {
+            ValidateContract(Name, SizeInPoints);
+        }
+
+        #endregion
+#endif
     }
 }
